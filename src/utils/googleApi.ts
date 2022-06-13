f904be7c559609f15b99c55596c3c5ff852f1038
Gustavo <gu.machado.oliveira@gmail>
2022-06-13 18:54:36 -0300

    feat: create api connection and api middleware

---
+++ src/utils/googleApi.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const googleApi = axios.create({
+  baseURL: `https://maps.googleapis.com/maps/api/geocode`,
+});
+
+export { googleApi };
