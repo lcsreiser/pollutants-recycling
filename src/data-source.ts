30697c798156afa1bc47cb7a9caf5dd020ad8ad6
Lucas <lcssreiser@gmail.com>
2022-06-14 22:51:54 -0300

    done

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,6 +8,7 @@ export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
   logging: false,
+  ssl: { rejectUnauthorized: false },
   entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
   migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
 });
